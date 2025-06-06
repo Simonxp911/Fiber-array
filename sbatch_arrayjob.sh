b49defcb989fdf29b40aa311cbed870229d5f698
Simonxp911 <simonxp911@gmail.com>
2025-06-06 12:42:21 +0200

    update sbatch for test

--- sbatch_arrayjob.sh
+++ sbatch_arrayjob.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH -J imperf_atom_cav
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=2:00:00
-#SBATCH --mem=1GB
-#SBATCH --array=1-6
-####SBATCH --account=p70623
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0:10:00
+#SBATCH --mem=1MB
 
 
 echo "========= started at `date` =========="
@@ -13,22 +11,19 @@ echo "========= started at `date` =========="
 
 # Load Julia
 module purge
-module load julia/1.8.2-gcc-12.2.0-zn5hofz
-# module load julia/1.10.4-gcc-12.2.0-b3e3xer   
+# module load julia/1.8.2-gcc-12.2.0-zn5hofz
+module load julia/1.10.4-gcc-12.2.0-b3e3xer   
 
-# Input filename
-INPUT=input_ff_0.9,0.95,6_pur_0.1_w0r_5.0_L_6.2_rad_6.txt
 
 # Print some info
 echo "  "
-echo "  INPUT = $INPUT"
 echo "  mem   = $SLURM_MEM_PER_NODE MB"
-echo "  array = $SLURM_ARRAY_TASK_MIN - $SLURM_ARRAY_TASK_MAX"
+echo "  #proc = $SLURM_CPUS_PER_TASK"
 
 
 # Run code 
-julia main.jl $INPUT $DATA $SLURM_ARRAY_TASK_ID
-
+# julia main.jl $INPUT $DATA $SLURM_ARRAY_TASK_ID
+mpiexec -n $SLURM_CPUS_PER_TASK julia --project test.jl
 
 echo "========= job finished at `date` =========="
 
