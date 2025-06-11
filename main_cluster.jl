95e051b9e6de1367ba586eee0543478f6454f6cf
Simonxp911 <simonxp911@gmail.com>
2025-06-11 15:20:38 +0200

    minor changes to main_cluster.jl

--- main_cluster.jl
+++ main_cluster.jl
@@ -19,12 +19,16 @@ include(ARGS[2])
 # ================================================
 function main()
     # Define system parameters
-    SP = define_SP()
+    if myRank == root SP = define_SP() else SP = nothing end
+    SP = MPI.bcast(SP, root, comm)
     
     
-    prepare_Im_Grm_trans(SP)
-    # plot_imperfectArray_transmission_vs_Δ(SP)
-    
+    task = ARGS[3]
+    if task == "prepare_Im_Grm_trans"
+        prepare_Im_Grm_trans(SP)
+    elseif task == "imperfectArray_transmission_vs_Δ"
+        imperfectArray_transmission_vs_Δ(SP)
+    end
     return nothing
 end
 
@@ -33,34 +37,55 @@ end
 #   Generate figures
 # ================================================
 function prepare_Im_Grm_trans(SP)
-    totalNumberOfJobs = SP.N^2
-    myStartIndex, myEndIndex = myStartIndex_and_myEndIndex(totalNumberOfJobs)
-    
-    index = 1
-    for j in 1:SP.N, i in 1:SP.N
-        if myStartIndex <= index <= myEndIndex
-            println("My rank is $myRank")
-            println("I am working on index = $index")
-            if i <= j     Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (0, 0), 1, true, SP.abstol) end
-            for α in 1:3
-                          Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (1, 0), α, true, SP.abstol)
-                          Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (2, 0), α, true, SP.abstol)
-                if i == j Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (1, 1), α, true, SP.abstol) end
+    # For arrays on the form [const, 0, zn] this is enough (as long as they are ordered according to their zn)
+    if SP.arrayType ∈ ("1Dchain", "randomZ")
+        totalNumberOfJobs = SP.N
+        myStartIndex, myEndIndex = myStartIndex_and_myEndIndex(totalNumberOfJobs)
+        index = 1
+        for i in 1:SP.N
+            if myStartIndex <= index <= myEndIndex
+                println("My rank is $myRank, I am working on index = $index")
+                            Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[1], (0, 0), 1, true, SP.abstol_Im_Grm_trans)
+                for α in 1:3
+                            Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[1], (1, 0), α, true, SP.abstol_Im_Grm_trans)
+                            Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[1], (0, 1), α, true, SP.abstol_Im_Grm_trans)
+                            Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[1], (2, 0), α, true, SP.abstol_Im_Grm_trans)
+                            Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[1], (0, 2), α, true, SP.abstol_Im_Grm_trans)
+                    if i == 1 Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[1], (1, 1), α, true, SP.abstol_Im_Grm_trans) end
+                end
             end
+            index += 1
         end
-        index += 1
+    
+    else
+        totalNumberOfJobs = SP.N^2
+        myStartIndex, myEndIndex = myStartIndex_and_myEndIndex(totalNumberOfJobs)
+        index = 1
+        for j in 1:SP.N, i in 1:SP.N
+            if myStartIndex <= index <= myEndIndex
+                println("My rank is $myRank, I am working on index = $index")
+                if i <= j     Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (0, 0), 1, true, SP.abstol_Im_Grm_trans) end
+                for α in 1:3
+                              Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (1, 0), α, true, SP.abstol_Im_Grm_trans)
+                              Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (2, 0), α, true, SP.abstol_Im_Grm_trans)
+                    if i == j Im_Grm_trans(SP.fiber, ωa, SP.array[i], SP.array[j], (1, 1), α, true, SP.abstol_Im_Grm_trans) end
+                end
+            end
+            index += 1
+        end
     end
 end
 
 
-function plot_imperfectArray_transmission_vs_Δ(SP)
-    if SP.n_inst == 1 throw(ArgumentError("plot_imperfectArray_transmission_vs_Δ requires n_inst > 1")) end
+function imperfectArray_transmission_vs_Δ(SP)
+    if SP.n_inst == 1 throw(ArgumentError("imperfectArray_transmission_vs_Δ requires n_inst > 1")) end
     
     totalNumberOfJobs = SP.n_inst
     myStartIndex, myEndIndex = myStartIndex_and_myEndIndex(totalNumberOfJobs)
     
     ts = []
     for index in myStartIndex:myEndIndex
+        println("My rank is $myRank, I am working on index = $index")
         if typeof(SP.d) == String
             σBα_scan = scan_steadyState(SP, SP.d, SP.array[index])
             push!(ts, calc_transmission.(Ref(SP), σBα_scan, Ref(SP.d), Ref(SP.array[index])))
@@ -70,14 +95,15 @@ function plot_imperfectArray_transmission_vs_Δ(SP)
         end
     end
     
-    ts = gather(ts, comm, root=root)
+    ts = MPI.gather(ts, comm, root=root)
     
     if myRank == root
-        postfix = get_postfix_imperfectArray_transmission(SP.Δ_specs, SP.ΔvariDescription, SP.dDescription, SP.να, SP.ηα, SP.incField_wlf, n_inst, SP.arrayDescription, SP.fiber.postfix)
+        postfix = get_postfix_imperfectArray_transmission(SP.Δ_specs, SP.ΔvariDescription, SP.dDescription, SP.να, SP.ηα, SP.incField_wlf, SP.n_inst, SP.arrayDescription, SP.fiber.postfix)
         filename = "T_phase" * postfix
         folder = "imperfectArray_T_phase/"
         
         # Prepare means and standard deviations of (squared) magnitudes and phases
+        ts = vcat(ts...)
         T_means, T_stds, phase_means, phase_stds = prep_imperfectArray_transmission(ts)
         formattedResult = vectorOfRows2Matrix([T_means, T_stds, phase_means, phase_stds])
         save_as_txt(formattedResult, saveDir * folder, filename)
