adb3113fb3d4a1fed9eb3f553bd4a10ca6920d05
Simonxp911 <simonxp911@gmail.com>
2025-06-12 12:28:04 +0000

    Small addition to make LinearAlgebra.jl work properly on the workstation

--- main_cluster.jl
+++ main_cluster.jl
@@ -1,7 +1,8 @@
 
 
 include("preamble.jl")
-const saveDir = ARGS[1] * "/fiber_array_data/"
+BLAS.set_num_threads(parse(Int, ARGS[1])) # Necessary on the workstation for LinearAlgebra to work properly
+const saveDir = ARGS[2] * "/fiber_array_data/"
 
 using MPI
 MPI.Init()
@@ -11,7 +12,7 @@ const myRank   = MPI.Comm_rank(comm)
 const commSize = MPI.Comm_size(comm)
 
 # Include the input file (which defines define_SP())
-include(ARGS[2])
+include(ARGS[3])
 
 
 # ================================================
@@ -23,7 +24,7 @@ function main()
     SP = MPI.bcast(SP, root, comm)
     
     
-    task = ARGS[3]
+    task = ARGS[4]
     if task == "prepare_Im_Grm_trans"
         prepare_Im_Grm_trans(SP)
     elseif task == "imperfectArray_transmission_vs_Δ"
